e66f5a35ce95062aad9b023152041421f26f7e22
KingHenrycruz <KingHenrycruz@gmail.com>
2023-10-30 19:38:51 +0000

    initial commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,16 +71,20 @@ class FileStorage:
 
     def get(self, cls, id):
         """ retrieves """
-        if cls in classes.values() and id and type(id) == str:
-            d_obj = self.all(cls)
-            for key, value in d_obj.items():
-                if key.split(".")[1] == id:
+        if cls in not in classes.values():
+            return None
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
                     return value
         return None
 
     def count(self, cls=None):
         """ counts """
+        all_class = classes.values()
         data = self.all(cls)
-        if cls in classes.values():
-            data = self.all(cls)
-        return len(data)
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        return count
